bbe36c7557171c9769a168a8afeedd4ff47eab9a
xyhh <niko@bogon.(none)>
2013-03-21 15:02:22 -0700

    Initial Commit

---
+++ Wephoto/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Wephoto
+//
+//  Created by xyhh on 13-3-21.
+//  Copyright (c) 2013年 bill wang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Wephoto/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Wephoto
+//
+//  Created by xyhh on 13-3-21.
+//  Copyright (c) 2013年 bill wang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
